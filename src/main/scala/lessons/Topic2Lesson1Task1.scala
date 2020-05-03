8114983b3ff60d469f0ce411e917679ae742c1ef
kochetova <daria.d.igumnova@gmail.com>
2020-05-03 12:38:47 +0300

    The 2.1-st lesson

---
+++ src/main/scala/lessons/Topic2Lesson1Task1.scala
@@ -0,0 +1,13 @@
+package lessons
+
+import scala.io.StdIn.readLine
+
+object Topic2Lesson1Task1 {
+  def main(args: Array[String]): Unit={
+
+    val name = readLine()
+
+    println("Hi, " + name + "!")
+
+  }
+}
