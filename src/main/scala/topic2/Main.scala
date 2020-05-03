6c05d18c40f0fea4fe0c30269c77f002a063e03b
kochetova <daria.d.igumnova@gmail.com>
2020-05-03 14:12:03 +0300

    2.3.1 and 2.3.2

---
+++ src/main/scala/topic2/Main.scala
@@ -0,0 +1,29 @@
+package topic2
+
+import scala.io.StdIn._
+
+object Main {
+  def main(args: Array[String]): Unit={
+    //2.1.1
+    {
+      val name = readLine()
+
+    println("Hi, " + name + "!")
+    }
+    //2.3.1
+    {
+      val FirstNumber = readInt()
+      val SecondNumber = readInt()
+
+      println(FirstNumber - SecondNumber)
+    }
+    //2.3.2
+    {
+      val FirstNumber = readInt()
+      val SecondNumber = readInt()
+      val ThirdNumber = readInt()
+
+      println(FirstNumber * SecondNumber * ThirdNumber)
+    }
+  }
+}
